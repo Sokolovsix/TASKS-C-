2e5ea6ed54022946f6a1771429f64ea7d60976c4
evgeny <sokolovsix@mail.ru>
2022-07-09 20:53:30 +0400

    add Task 001 Quatro

---
+++ Task001_Quatro/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a * a == b) 
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
