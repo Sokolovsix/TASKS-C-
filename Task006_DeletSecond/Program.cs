c08d0daab0c75ea5bbda494ab236442eb0093022
evgeny <sokolovsix@mail.ru>
2022-07-19 15:56:17 +0400

    add Task 006 Delet Second

---
+++ Task006_DeletSecond/Program.cs
@@ -0,0 +1,6 @@
+﻿int randomNumber = new Random ().Next(99 , 1000);
+
+int firstDigit = randomNumber / 100;
+int secondDigit = randomNumber % 10;
+
+Console.WriteLine($"{randomNumber} -> {firstDigit+""+secondDigit}");
