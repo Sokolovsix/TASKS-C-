97dfd583a3b3dcb8c8a16841a019df7895d1ed96
evgeny <sokolovsix@mail.ru>
2022-07-23 21:21:02 +0400

    add Task 007 Multiple

---
+++ Task007_Multiple/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int number3 = number1 % number2;
+if (number3 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Остаток" + number3);
+}
+
