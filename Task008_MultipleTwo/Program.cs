47aaf445b916272f4658c85842cc57b2cf560cb7
evgeny <sokolovsix@mail.ru>
2022-07-25 08:57:56 +0400

    add Task 008 Multiple Two

---
+++ Task008_MultipleTwo/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 7 == 0 && number1 % 23 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
