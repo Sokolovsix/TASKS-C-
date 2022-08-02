c86040c6b25aea706e79d8a68b9cc12df629f8a5
evgeny <sokolovsix@mail.ru>
2022-08-02 19:46:39 +0400

    add Task's

---
+++ Seminar3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Принимаем X и Y не равные нулю и определяем номер четверти в плоскости
+
+while(true)
+
+{
+    Console.Clear();
+    int x = InputInt("Введите X: ");
+    int y = InputInt("Введите Y: ");
+
+    if(x == 0 || y == 0)
+    {
+        Console.WriteLine("X и Y не должны быть равны 0 ");
+        Console.ReadKey();
+        continue;
+    }
+
+    if(x > 0 && y > 0)
+    {
+        Console.WriteLine("1 четверть ");
+    }
+    else if(x < 0 && y > 0)
+    {
+        Console.WriteLine("2 четверть");
+    }
+    else if(x < 0 && y < 0)
+    {
+        Console.WriteLine("3 четверть");
+    }
+    else if(x > 0 && y < 0)
+    {
+        Console.WriteLine("4 четверть");
+    }
+
+    break;
+}
+
+int InputInt(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
